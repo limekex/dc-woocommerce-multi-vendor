3188381f90a2b0e6b7dc458638d0e55246956a86
cool-raju <rajsekhar@dualcube.com>
2022-04-11 13:21:18 +0530

    Plugin updated with version 3.8.11.2

--- dc_product_vendor.php
+++ dc_product_vendor.php
@@ -4,10 +4,10 @@
  * Plugin URI: https://wc-marketplace.com/
  * Description: A Free Extension That Transforms Your WooCommerce Site into a Marketplace.
  * Author: WC Marketplace
- * Version: 3.8.11.1
+ * Version: 3.8.11.2
  * Author URI: https://wc-marketplace.com/
  * Requires at least: 4.4
- * Tested up to: 5.9.2
+ * Tested up to: 5.9.3
  * WC requires at least: 3.0
  * WC tested up to: 6.3.1
  *
